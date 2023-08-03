91b22d879fb4058f33e31e5e544563a80737bc80
Jacob Yoo <tolearneverything123@gmail.com>
2023-08-03 07:27:54 -0400

    Add csv data and checks multiple products
    CSV data holds the urls and prices of products to be searched. Scrapes each url in a loop.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()
 
@@ -12,27 +13,41 @@ from_email = os.getenv("EMAIL")
 to_email = os.getenv("TO_EMAIL")
 app_pass = os.getenv("APP_PASS")
 
-
 os.environ['PATH'] += r'/Users/jacobyoo/Downloads/chromedriver_mac_arm64/chromedriver'
-website = 'https://www.pluginboutique.com/product/2-Effects/44-Saturation/3016-RC-20-Retro-Color'
-driver = webdriver.Chrome()
-driver.get(website)
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=options)
+df_dict = pd.read_csv('all_prices.csv').to_dict("list")
+original_df = pd.read_csv('all_prices.csv')
 
-time.sleep(5)
-soup = BeautifulSoup(driver.page_source, 'lxml')
+for index, row in original_df.iterrows():
+    driver.get(row['url'])
+    
+    time.sleep(5)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
 
-price = float(soup.find('dd').text.strip("$"))
-product = soup.find('h1').text
+    price = float(soup.find('dd').text.strip("$"))
+    product = soup.find('h1').text
 
-if price < 99.95:
-    em = EmailMessage()
-    em["From"] = from_email
-    em["To"] = to_email
-    em["Subject"] = f"DEAL FOR {product} ON PLUGIN BOUTIQUE WAS $99.95 and now is ${price}!"
-    em.set_content(f"Dear Me,\n\nThe current price of {product} used to be $99.95 and is now ${price}!\n\nFrom,\n\n Me")
+    if product not in df_dict['product']:
+        index_of_url = df_dict['url'].index(row['url'])
+        df_dict['product'][index_of_url] = product
+        df_dict['price'][index_of_url] = price
+    elif price < float(row['price']):
+        em = EmailMessage()
+        em["From"] = from_email
+        em["To"] = to_email
+        em["Subject"] = f"DEAL FOR {product} ON PLUGIN BOUTIQUE WAS ${row['price']} AND NOW IS ${price}!"
+        em.set_content(f"Dear Me,\n\nThe current price of {product} used to be ${row['price']} and is now ${price}!\n\nFrom,\n\n Me")
 
-    context = ssl.create_default_context() # secures the message!
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(from_email, app_pass)
-        smtp.sendmail(from_email, to_email, em.as_string())
+        context = ssl.create_default_context() # secures the message!
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(from_email, app_pass)
+            smtp.sendmail(from_email, to_email, em.as_string())
+        df_dict['price'][df_dict['product'].index(product)] = price
+    else:
+        df_dict['price'][df_dict['product'].index(product)] = price
+
+df = pd.DataFrame.from_dict(df_dict)
+df.to_csv('all_prices.csv', index=False)
     
